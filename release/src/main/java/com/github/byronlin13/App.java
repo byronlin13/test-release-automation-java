170653c0771c7d121ed33a3a34d16fac3caa8e6b
Byron Lin <byronl@bitquilltechj.com>
2021-05-09 01:45:09 -0700

    Add release folder

---
+++ release/src/main/java/com/github/byronlin13/App.java
@@ -0,0 +1,18 @@
+package com.github.byronlin13;
+
+import com.github.byronlin13.quickstart.Operation;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Operation operation = new Operation();
+
+        System.out.println(operation.add(1,2));
+        System.out.println("HELLO WORLD");    }
+}
